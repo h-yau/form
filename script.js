590dee2174e85db01bce373610809764c2d100a0
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-05 17:10:10 -0700

    Add simple logic and checking conditions

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
-console.log('hi');
+const form = document.querySelector('form');
+const email = form.querySelector('#email');
+const password = form.querySelector('#password');
+const confirmPassword = form.querySelector('#confirm-password');
+const submitButton = document.querySelector('button');
+
+submitButton.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+email.addEventListener('input', () => {
+  email.setCustomValidity('');
+  validateEmail();
+});
+
+password.addEventListener('input', () => {
+  password.setCustomValidity('');
+  validatePassword();
+});
+
+const validatePassword = () => {
+  if (
+    !checkIfLengthGreaterThan(8, password) ||
+    !checkIfAtLeastOneSpecialChar(password)
+  ) {
+    password.reportValidity();
+  }
+};
+
+const validateEmail = () => {
+  if (!checkIfLengthGreaterThan(5, email) || !checkIfValidEmail(email)) {
+    email.reportValidity();
+  }
+};
+
+const checkIfLengthGreaterThan = (number, inputElement) => {
+  if (inputElement.value.length < number) {
+    inputElement.setCustomValidity('Too short man!');
+    return false;
+  }
+  return true;
+};
+
+const checkIfAtLeastOneSpecialChar = (inputElement) => {
+  const specialChars = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (!specialChars.test(inputElement.value)) {
+    inputElement.setCustomValidity("Where's my special character?");
+    return false;
+  }
+  return true;
+};
+
+const checkIfValidEmail = (inputElement) => {
+  if (!/@/.test(inputElement.value)) {
+    inputElement.setCustomValidity("Where's my @? Not cool!");
+    return false;
+  } else if ('@' == inputElement.value[inputElement.value.length - 1]) {
+    inputElement.setCustomValidity(
+      "You know that's not a valid email address."
+    );
+    return false;
+  }
+  return true;
+};
